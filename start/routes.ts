895dfa1c85b8717544069a700dbf297679ee2ac9
rohitjames88 <rohitjames88@gmail.com>
2026-03-13 13:46:01 +0530

    Change routes to import files and render html dummy files

--- start/routes.ts
+++ start/routes.ts
@@ -12,6 +12,7 @@ import { controllers } from '#generated/controllers'
 import router from '@adonisjs/core/services/router'
 import app from '@adonisjs/core/services/app'
 import fs from 'node:fs/promises'
+
 router.on('/').render('pages/home').as('home')
 
 router
@@ -32,6 +33,8 @@ router
 
 router
   .get('/movies/:slug', async (ctx) => {
-    return ctx.view.render('pages/movies', { movie: ctx.params.slug })
+    const url = app.makeURL(`resources/movies/${ctx.params.slug}.html`)
+    let movie = await fs.readFile(url, 'utf-8')
+    return ctx.view.render('pages/movies', { movie })
   })
   .as('movies.show')
